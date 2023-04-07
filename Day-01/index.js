6b4070019ac949b3efa02b1acdfd8282abc2914d
sung <shuh012981@gmail.com>
2023-04-08 01:20:56 +0800

    day 1 updated

---
+++ Day-01/index.js
@@ -0,0 +1,21 @@
+const arr = [1,2,3];
+
+arr.forEach(alert);
+
+
+/*
+Variable Hoisting
+- Variable declaration is executed first before runtime.
+*/
+
+//camelCase   <- variables, functions
+var firstName;
+
+//snake_case
+var first_name;
+
+//PascalCase    <- class
+var FirstName;
+
+//typeHungarianCase
+var strFirstName; // type + identifier
